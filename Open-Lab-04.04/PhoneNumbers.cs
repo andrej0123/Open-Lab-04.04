3363113c63160f62bb355672b2f2742e0ab6d3de
Andrej Svrček <nepoviem251@gmail.com>
2021-11-12 21:24:15 +0100

    takes an array of 10 integers and formats them into a phone number

--- Open-Lab-04.04/PhoneNumbers.cs
+++ Open-Lab-04.04/PhoneNumbers.cs
@@ -6,7 +6,22 @@ namespace Open_Lab_04._04
     {
         public string FormatPhoneNumber(int[] numbers)
         {
-            throw new NotImplementedException();
+                       
+            string a = numbers[0].ToString();
+            string b = numbers[1].ToString();
+            string c = numbers[2].ToString();
+            string d = numbers[3].ToString();
+            string e = numbers[4].ToString();
+            string f = numbers[5].ToString();
+            string g = numbers[6].ToString();
+            string h = numbers[7].ToString();
+            string i = numbers[8].ToString();
+            string j = numbers[9].ToString();
+
+            string answer = String.Concat("(", a, b, c, ")", " ", d, e, f, "-", g, h, i, j);
+            
+            return answer;
+          
         }
     }
 }
